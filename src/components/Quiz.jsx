fa614fc46836c0d4f9fafab60f09981b4ab52d0c
Lyrecoph <lyrecoph30@gmail.com>
2024-08-22 22:11:43 +0100

    Moving Logic To Components That Actually Need It and Setting Different Timers Based On The Selected Answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -11,44 +11,28 @@ import Question from "./Question.jsx";
 
 
 export default function Quiz(){
-    // Etat qui gère la réponse actuelle
-    const [answerState, setAnswerState] = useState('');
     // Etat qui gère l'enregistrement des réponses sélectionnées par l'utilisateur
     const [userAnswers, setUserAnswers] = useState([]);
 
     // dérive l'index de la question à partir des éléments de réponse 
     // se trouvant dans le tableau de question
-    const activeQuestionIndex = 
-        answerState === '' ? userAnswers.length : userAnswers.length - 1;
+    const activeQuestionIndex = userAnswers.length;
     
     // le nbre de réponse doit être égal au nbre de question
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback (
         function handleSelectAnswer(selectedAnswer){
-        // permet de changer la couleur de la réponse sélectionné
-        setAnswerState('answered')
         // permet de stocker la réponses sélectionnées dans un tableau
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer]
         });
-
-        setTimeout(() => {
-            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
-                setAnswerState('correct');
-            } else{
-                setAnswerState('wrong');
-            }
-
-            setTimeout(() => {
-                setAnswerState('');
-            }, 2000);
-        }, 1000);
-    }, [activeQuestionIndex]);
+    }, []);
 
     const handleSkipAnswer = useCallback(
         () => handleSelectAnswer(null), 
-        [handleSelectAnswer]);
+
+    [handleSelectAnswer]);
 
     if(quizIsComplete){
         return (
@@ -63,10 +47,7 @@ export default function Quiz(){
         <div id="quiz">
             <Question 
                 key={activeQuestionIndex}
-                questionText={QUESTIONS[activeQuestionIndex].text}
-                answers={QUESTIONS[activeQuestionIndex].answers}
-                answerState={answerState}
-                selectedAnswer={userAnswers[userAnswers.length - 1]}
+                indexques={activeQuestionIndex}
                 onSelectAnswer={handleSelectAnswer}
                 onSkipAnswer={handleSkipAnswer}
             />
