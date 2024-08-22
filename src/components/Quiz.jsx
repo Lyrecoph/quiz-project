c43f5f80d9409d46a069e832ea2362282a47182c
Lyrecoph <lyrecoph30@gmail.com>
2024-08-23 00:39:47 +0100

    To be corrected

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,8 +6,8 @@
 import { useCallback, useState } from "react";
 
 import QUESTIONS from '../questions.js';
-import quizCompleteImg from '../assets/quiz-complete.png';
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 
 export default function Quiz(){
@@ -36,10 +36,7 @@ export default function Quiz(){
 
     if(quizIsComplete){
         return (
-            <div id="summary">
-                <img src={quizCompleteImg} alt="Trophy icon" />
-                <h2>Quiz Completed !</h2>
-            </div>
+            <Summary userAnswers={userAnswers} />
         )
     }
 
